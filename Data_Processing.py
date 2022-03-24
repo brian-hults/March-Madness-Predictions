27e660fd210da5e1ec8bd231b900e2aa755cfdbf
Brian Hults <hultsb@gmail.com>
2022-03-24 16:04:26 -0400

    Updated home/away team status handling

--- Data_Processing.py
+++ Data_Processing.py
@@ -9,24 +9,20 @@ NCAA Mens Basketball - Data Processing
 
 # Install pacakges
 import pandas as pd
+import numpy as np
 import time
 import datetime
-from tqdm import tqdm
+from tqdm import tqdm # this is the progress bar module
 from SQL_Utils import SQL_Utils
 from sportsipy.ncaab.teams import Teams
-from sklearn.linear_model import LassoCV
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 
 class Data_Processing:
     def __init__(self):
         self.seed = 37
         self.today = datetime.date.today()
-        self.unavailable = ['brown','columbia','cornell','dartmouth','harvard',
-                            'maryland-eastern-shore','pennsylvania','princeton',
-                            'yale','cal-poly']
+        self.unavailable = [] # ['brown','columbia','cornell','dartmouth','harvard','maryland-eastern-shore','pennsylvania','princeton','yale','cal-poly']
         self.sql = SQL_Utils()
-        self.teams = Teams()
+        self.teams = Teams(year='2022')
         
     
     def update_schedule_db(self):
@@ -38,13 +34,16 @@ class Data_Processing:
             last_updated = datetime.date.fromisoformat(pd.read_sql_table('updated_date', con=self.sql.engine)['0'][0].strftime('%Y-%m-%d'))
         except:
             last_updated = False
+            
+        print("Last Updated: ", last_updated)
         
         # Initialize empty dataframe to combine schedules into
         combined_df = pd.DataFrame()
         
         # Iterate through teams
         for team in tqdm(self.teams):
-            # print(team.abbreviation)
+            #if team.abbreviation.lower() == 'kentucky':
+            #print(team.abbreviation)
             # Check if a team is in the list of unavailable records
             if team.abbreviation.lower() in self.unavailable:
                 continue
@@ -52,46 +51,27 @@ class Data_Processing:
             elif last_updated:
                 # If the last update was before today
                 if datetime.date.today() > last_updated:
-                    try:
-                        schedule = team.schedule.dataframe_extended
-                        # Convert the date column to a datetime object to use for filtering
-                        # Source: https://stackoverflow.com/questions/26387986/strip-time-from-an-object-date-in-pandas
-                        schedule['date'] = pd.to_datetime(schedule['date'], format='%B %d, %Y').apply(lambda x:x.date())
-                        
-                        # Filter out games that were before the last updated date
-                        schedule = schedule[schedule['date'] > last_updated]
-                        
-                        # Determine team's home/away status
-                        team_status = pd.Series(schedule.index.str.contains(team.abbreviation.lower(), regex=False),
-                                                index=schedule.index,
-                                                name='home_team').replace({True:'Home', False:'Away'})
-                        # Add team status to the dataframe
-                        schedule = pd.concat([team_status, schedule], axis=1)
-                        
-                        # Convert Nan values to zeros
-                        schedule.fillna(0, axis=0, inplace=True)
-                        
-                        # Push team schedule to database
-                        schedule.to_sql(team.abbreviation.lower(), con=self.sql.engine, if_exists='append')
-                        
-                        # Add records to the combined dataframe
-                        combined_df = pd.concat([combined_df, schedule], axis=0)
-                    except:
-                        print('\n', team.abbreviation, ' Failed to update team schedule in database!')
-                else:
-                    continue
-
-            else:
-                try:
+                    #try:
                     schedule = team.schedule.dataframe_extended
                     # Convert the date column to a datetime object to use for filtering
                     # Source: https://stackoverflow.com/questions/26387986/strip-time-from-an-object-date-in-pandas
                     schedule['date'] = pd.to_datetime(schedule['date'], format='%B %d, %Y').apply(lambda x:x.date())
                     
+                    # Filter out games that were before the last updated date
+                    schedule = schedule[schedule['date'] > last_updated]
+                    
+                    # Get the two team names playing
+                    team_matchups = schedule['winning_abbr','losing_abbr'].replace({team.abbreviation: np.nan})
+                    opponents = team_matchups['winning_abbr'].combine_first(team_matchups['losing_abbr'])
+                    print(opponents)
+                    
                     # Determine team's home/away status
                     team_status = pd.Series(schedule.index.str.contains(team.abbreviation.lower(), regex=False),
                                             index=schedule.index,
-                                            name='home_team') #.replace({True:'Home', False:'Away'})
+                                            name='home_team').replace({True:'Home', False:'Away'})
+                    
+                    # Add columns for the Home and Away team abbreviations
+                    home_team = team_status.replace({'Home': team.abbreviation,'Away': opponents})
                     
                     # Add team status to the dataframe
                     schedule = pd.concat([team_status, schedule], axis=1)
@@ -99,12 +79,44 @@ class Data_Processing:
                     # Convert Nan values to zeros
                     schedule.fillna(0, axis=0, inplace=True)
                     
-                    schedule.to_sql(team.abbreviation.lower(), con=self.sql.engine, if_exists='replace')
+                    # Push team schedule to database
+                    schedule.to_sql(team.abbreviation.lower(), con=self.sql.engine, if_exists='append')
                     
                     # Add records to the combined dataframe
-                    combined_df = pd.concat([combined_df, schedule])
-                except:
-                    print('\n', team.abbreviation, ' Failed to write team schedule to database!')
+                    combined_df = pd.concat([combined_df, schedule], axis=0)
+# =============================================================================
+#                     except:
+#                         print('\n', team.abbreviation, ' Failed to update team schedule in database!')
+# =============================================================================
+                else:
+                    continue
+
+            else:
+                #try:
+                schedule = team.schedule.dataframe_extended
+                # Convert the date column to a datetime object to use for filtering
+                # Source: https://stackoverflow.com/questions/26387986/strip-time-from-an-object-date-in-pandas
+                schedule['date'] = pd.to_datetime(schedule['date'], format='%B %d, %Y').apply(lambda x:x.date())
+                
+                # Determine team's home/away status
+                team_status = pd.Series(schedule.index.str.contains(team.abbreviation.lower(), regex=False),
+                                        index=schedule.index,
+                                        name='home_team') #.replace({True:'Home', False:'Away'})
+                
+                # Add team status to the dataframe
+                schedule = pd.concat([team_status, schedule], axis=1)
+                
+                # Convert Nan values to zeros
+                schedule.fillna(0, axis=0, inplace=True)
+                
+                schedule.to_sql(team.abbreviation.lower(), con=self.sql.engine, if_exists='replace')
+                
+                # Add records to the combined dataframe
+                combined_df = pd.concat([combined_df, schedule])
+# =============================================================================
+#                 except:
+#                     print('\n', team.abbreviation, ' Failed to write team schedule to database!')
+# =============================================================================
         
         # Reset index to a column so we can remove duplicates
         combined_df.reset_index(inplace=True)
@@ -114,10 +126,12 @@ class Data_Processing:
         cleaned_combined_df = combined_df.drop_duplicates(subset='Date_Home_Team_Index').set_index('Date_Home_Team_Index')
         
         # Write combined dataframe to the database
-        cleaned_combined_df.to_sql('cleaned_combined_schedule', con=self.sql.engine, if_exists='append')
+        cleaned_combined_df.to_sql('cleaned_combined_schedule',
+                                   con=self.sql.engine,
+                                   if_exists='replace')
         
         # Record the last date the schedule database was updated
-        updated_date = pd.Series(data={'date': datetime.date.today()})
+        updated_date = pd.Series(data={'date': datetime.date.today()}) # datetime.date(2021,11,1)})
         updated_date.to_sql('updated_date', con=self.sql.engine, if_exists='replace')
 
         # Print runtime
@@ -129,7 +143,19 @@ db_updater = Data_Processing()
 # Update team schedules
 db_updater.update_schedule_db()
 
-
+# =============================================================================
+# NCAAB Boxscore noted issue on Github -- Source: https://github.com/roclark/sportsipy/issues/674
+# Fix is to edit the boxscore.py local file per the screencap at that source page. Reference code below:
+# boxscore.py line 388:
+# 
+# records = boxscore(BOXSCORE_SCHEME[field]).items()
+# records = [x.text() for x in records if x.text() != '']
+# 
+# if len(records) > index:
+#     return records[index]
+# else:
+#     return ''
+# =============================================================================
 
 
 
